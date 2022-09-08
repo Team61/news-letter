fbe171bf5c1ceac113f931efe0305f11f0e2cc57
daniel-cardone <dcard278@gmail.com>
2022-09-07 21:12:05 -0400

    begin styling, add semantic structure and proper minifier

--- compile.js
+++ compile.js
@@ -1,13 +1,15 @@
 const fs = require("fs");
 const pug = require("pug");
+const minify = require("html-minifier").minify;
 
 const data = fs.readFileSync("data.txt", "utf8");
 const messages = data.split("<>").map(message => {
   const [date, heading, body] = message.trim().split("\n").map(part => part.trim());
   return { date: new Date(date), heading, body };
-});
+}); // TODO: uh oh will I be able to put pictures
 
 const compiler = pug.compileFile("template.pug");
 const html = compiler({ messages });
+const minified = minify(html, { minifyCSS: true });
 
-fs.writeFileSync("newsletter.html", html);
+fs.writeFileSync("newsletter.html", minified);
