360ff8f0949ef346c6a1c32fa44001b0e877248c
daniel-cardone <dcard278@gmail.com>
2022-09-09 12:58:55 -0400

    finish styling to complete first release

--- compile.js
+++ compile.js
@@ -5,8 +5,8 @@ const minify = require("html-minifier").minify;
 const data = fs.readFileSync("data.txt", "utf8");
 const messages = data.split("<>").map(message => {
   const [date, heading, body] = message.trim().split("\n").map(part => part.trim());
-  return { date: new Date(date), heading, body };
-}); // TODO: uh oh will I be able to put pictures
+  return { date: new Date(date) || null, heading, body };
+});
 
 const compiler = pug.compileFile("template.pug");
 const html = compiler({ messages });
